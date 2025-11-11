68a2b8c0a7cd19d98dc37ef22c8e9eca6941776f
ggan-au <glenganderton@proton.me>
2025-11-12 02:06:40 +1100

    EXERCISE-LL-APPEND completed.

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -20,6 +20,18 @@ public class LinkedList {
         }
     }
 
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
     public void printList() {
         Node temp = head;
         while (temp != null) {
--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -4,12 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(4);
+        LinkedList myLinkedList = new LinkedList(1);
 
         myLinkedList.getHead();
         myLinkedList.getTail();
         myLinkedList.getLength();
 
+        System.out.println("\nLinked List:");
+        myLinkedList.printList();
+
+        myLinkedList.append(2);
+
+        System.out.println("=============================");
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
         System.out.println("\nLinked List:");
         myLinkedList.printList();
 
