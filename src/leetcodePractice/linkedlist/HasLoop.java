7d22dab50090ddd439c9a04fbb2768435f280274
ggan-au <glenganderton@proton.me>
2025-11-13 19:18:00 +1100

    LC - HasLoop - Complete

---
+++ src/leetcodePractice/linkedlist/HasLoop.java
@@ -0,0 +1,13 @@
+package leetcodePractice.linkedlist;
+
+public class HasLoop {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.append(5);
+
+        System.out.println("Does it have a loop: " +  myLinkedList.hasLoop());
+    }
+}
